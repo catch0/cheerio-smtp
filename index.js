dc37da458c69ebc7bdb1000708d2b795617d668d
Spence Peacock <spencerpeacock@gmail.com>
2017-10-21 16:04:36 -0500

    nodemail works but sends [Object object]

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
 require('dotenv').load();
+var fs = require('fs');
 var path = require('path');
 // var config = JSON.parse(fs.readFileSync("package.json"));
-var cheerio = require('cheerio');
+var Nightmare = require('nightmare');
+      nightmare = Nightmare();
 var request = require('request');
 var jquery = require('jquery');
 var account = process.env.ACCOUNT ;
 var password = process.env.PASS ;
-var smtp = process.env.SMTP_CALL ;
+var nodemailer = require('nodemailer');
 
-request('http://dallas.craigslist.org/search/bia?query=road&hasPic=1&postedToday=1', function(err, resp,body){
-  if(!err&& resp.statusCode == 200){
-    var bikes = [];
-    var $ = cheerio.load(body)
-    //create an array for bikes to be gathered
-    $('.hdrlnk').each(function(i, element){
-      var title = $(this).text()
-      var link = $(this).attr("href")
-      var bike = {
-        title: title,
-        link: link
-      };
-      console.log(bike.title)
-      console.log(bike.link)
-      console.log("")
-    });
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth:{
+    user: account,
+    pass: password
   }
 });
+
+nightmare.goto('http://dallas.craigslist.org/search/bia?query=road&hasPic=1&postedToday=1')
+//visits the city specified
+.wait(2000)
+.evaluate(function(){
+  var bikes = [];
+  //create an array for bikes to be gathered
+  $('.hdrlnk').each(function(){
+    item = {}
+    item["title"] = $(this).text()
+    item["link"] = $(this).attr("href")
+    bikes.push(item)
+  })
+  return bikes
+  //create the bike object with title and link then push it on i
+})
+ .end()
+.then(function(result){
+  const mailOptions = {
+  from:'lumanwalters@gmail.com',
+  to: 'spencerpeacock@gmail.com',
+  subject: "bikes",
+  html: '' + result 
+
+}
+  transporter.sendMail(mailOptions, function (err, info) {
+     if(err)
+       console.log(err)
+     else
+       console.log(info);
+  });
+})
 
 
 
